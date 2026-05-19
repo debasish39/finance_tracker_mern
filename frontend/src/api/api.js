cb5be0006cbea5f6d893a62c5fe648f7ff571480
Debasish Panda <156610494+debasish39@users.noreply.github.com>
2026-05-19 15:46:39 +0530

    API updated

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API =
   axios.create({
     baseURL:
-      "http://localhost:3000/api"
+      "https://finance-tracker-mern-o44s.onrender.com/api"
   });
 
 export default API;
